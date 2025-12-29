a0f80b4b004130d28a4d4c4c9495e35b895d8678
Edson Angoti Junior <angoti@gmail.com>
2025-12-29 13:51:15 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'racha-facil-v2';
+const urlsToCache = [
+  '/rachafacil/',
+  '/rachafacil/index.html',
+  '/rachafacil/styles.css',
+  '/rachafacil/app.js',
+  '/rachafacil/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
